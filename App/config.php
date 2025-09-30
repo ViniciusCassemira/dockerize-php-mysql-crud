5fa9c32d45b94f49d6da6f607a6b07c333777416
ViniciusCassemira <vinicius.cassemira@fatec.sp.gov.br>
2025-09-30 12:04:52 -0300

    fix: SQL table sintaxe

--- App/config.php
+++ App/config.php
@@ -3,7 +3,7 @@
 define('BASE_DIR', dirname(__FILE__, 2));
 define('VIEWS', BASE_DIR . '/App/View');
 
-$_ENV['db']['host'] = "meu_servidor_mysql:3306";
+$_ENV['db']['host'] = "meu_servidor_mysql";
 $_ENV['db']['user'] = "root";
 $_ENV['db']['pass'] = "etecjau";
 $_ENV['db']['database'] = "biblioteca";
